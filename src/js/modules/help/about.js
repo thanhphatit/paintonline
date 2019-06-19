3d3555fa75105a08ebedd665f99f7efd77e7b762
Dang Thanh Phat <zthanhphatz95@gmail.com>
2019-06-19 15:59:44 +0700

    Update

--- src/js/modules/help/about.js
+++ src/js/modules/help/about.js
@@ -10,7 +10,6 @@ class Help_about_class {
 	//about
 	about() {
 		var email = 'itblognote@gmail.com';	
-		
 		var settings = {
 			title: 'About',
 			params: [
@@ -19,14 +18,13 @@ class Help_about_class {
 				{title: "Version:", value: VERSION},
 				{title: "Description:", value: "Online Image Editor."},
 				{title: "Author:", value: 'ThanhPhat IT'},
-				{title: "Email:", html: '<a href="mailto:' + email + '">' + email + '</a>'},
+				{title: "Email:", html: '<a href="mailto:itblognote@gmail.com">itblognote@gmail.com</a>'},
 				{title: "GitHub:", html: '<a href="https://github.com/thanhphatit">https://github.com/thanhphatit</a>'},
 				{title: "Website:", html: '<a href="https://www.itblognote.com/">https://www.itblognote.com/</a>'},
 			],
 		};
 		this.POP.show(settings);
 	}
-
 }
 
 export default Help_about_class;

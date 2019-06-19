ac12237273e44fcfca61902be2ead0a701e9eab8
Dang Thanh Phat <zthanhphatz95@gmail.com>
2019-06-19 17:12:58 +0700

    Update about.js

--- src/js/modules/help/about.js
+++ src/js/modules/help/about.js
@@ -18,8 +18,8 @@ class Help_about_class {
 				{title: "Version:", value: VERSION},
 				{title: "Description:", value: "Online Image Editor."},
 				{title: "Author:", value: 'ThanhPhat IT'},
-				{title: "Email:", html: '<a href="mailto:itblognote@gmail.com">itblognote@gmail.com</a>'},
-				{title: "GitHub:", html: '<a href="https://github.com/thanhphatit">https://github.com/thanhphatit</a>'},
+				{title: "Email:", html: '<a href="'+email+'">'+email+'</a>'},
+				{title: "GitHub:", html: '<a href="https://github.com/thanhphatit">https://github.com/thanhphatit Chao</a>'},
 				{title: "Website:", html: '<a href="https://www.itblognote.com/">https://www.itblognote.com/</a>'},
 			],
 		};
